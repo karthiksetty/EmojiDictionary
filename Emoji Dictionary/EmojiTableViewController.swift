f782383979c979c9a2390f360d7145b3b0ca16fb
Unknown <karthik.setty@gmail.com>
2018-03-29 12:15:47 +0200

    Created table view and segues

---
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Karthik Setty on 28.03.18.
+//  Copyright © 2018 Karthik Setty. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+    
+    var emojis = ["💩", "😀", "😬", "🤗"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return emojis.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        cell.textLabel?.text = emojis[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "showDetail", sender: nil)
+    }
+
+
+}
